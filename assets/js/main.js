4c551fa05a2db0f67b586dff6600fe7f24b0386f
Space-Crusader25 <mar.syl.hernandez25@gmail.com>
2026-06-16 12:37:42 +0200

    info planete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Datas
 const longueur = 10;
 const hauteur = 10;
 const earth = {
@@ -17,38 +18,112 @@ const planets = [
     earth
 
 ];
-let drawnPlanets = {};
+const spaceship = {
+    x: 1,
+    y: 1
 
-const drawContent = (x, y) => {
-    
-   
 }
+let drawnPlanets = {};
+const mapBox = document.querySelector("#mapBox");
+const guiBox = document.querySelector("#gui");
 
+// Fonctions
 const drawMap = (w, h) => {
-
-    let body = document.querySelector("body");
-    let tablFin = `<table>`;
+    let tablFin = `<table class="map">`;
 
     for(let i = 0; i < h; i++){
         tablFin += `<tr>`;
         for(let j = 0; j < w; j++){
-            let content;
+            tablFin += "<td";
             planets.forEach(planet => {
                 if(planet.x == j && planet.y == i){
-                    content = `<img src="assets/img/${planet.name}.png" alt="${planet.name}">`;
+                    tablFin += ` style="background-image: url('assets/img/${planet.name}.png'); background-size: 100% 100%;"`;
                 }
             });
-            tablFin += `<td> ${content} </td>`;
+            tablFin += `>`;
+            if(spaceship.x == j && spaceship.y == i){
+                tablFin += `<img id="playerShip" src="assets/img/spaceship.png" alt="spaceship">`;
+            }
+            tablFin += `</td>`;
+            
         }
         tablFin += `</tr>`;
     }
     tablFin += `</table>`;
 
-    body.innerHTML += tablFin;
+    mapBox.innerHTML += tablFin;
 }
 
 const removeMap = () => {
-    const tableau = document.querySelector("table:last-of-type");
-    tableau.innerHTML = null;
-};
+    mapBox.innerHTML = null;
+}
+
+const restartMap = () => {
+    removeMap();
+    drawMap(longueur, hauteur);
+    return "Restarted Succesfully";
+}
+
 drawMap(longueur, hauteur);
+const shipImg = () => {
+    return document.querySelector("#playerShip");
+}
+const resetDir = () => {
+
+    shipImg().classList.remove("left", "right", "down");
+}
+const moveRight = () => {
+    if(spaceship.x < longueur-1) {
+        spaceship.x++;
+    }else{
+        spaceship.x = 0;
+    }
+    restartMap();
+    resetDir();
+    shipImg().classList.add("right");
+}
+
+const moveDown = () => {
+    if(spaceship.y < hauteur-1) {
+        spaceship.y++;
+    }else{
+        spaceship.y = 0;
+    }
+    restartMap();
+    resetDir();
+    shipImg().classList.add("down");
+}
+
+const moveLeft = () => {
+    if(spaceship.x > 0) {
+        spaceship.x--;
+    }else{
+        spaceship.x = longueur-1;
+    }
+    restartMap();
+    resetDir();
+    shipImg().classList.add("left");
+}
+
+const moveUp = () => {
+
+    if(spaceship.y > 0) {
+        spaceship.y--;
+    }else{
+        spaceship.y = hauteur-1;
+    }
+    restartMap();
+    resetDir();
+}
+const updateGui = () => {
+    planets.forEach(planet => {
+        if(planet.x == spaceship.x && planet.y == spaceship.y){
+            guiBox.innerHTML = planet.name;
+        }
+    });
+}
+const update = () => {
+    updateGui();
+}
+
+setInterval(update, 1000);
